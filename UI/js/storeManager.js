f7daf73ed0aad259d720681d47b55dcb044e6281
Sam Lando <samuel.lando@gmail.com>
2018-10-10 23:39:10 +0300

    [#161037324] ft-store-attendant-add2cart-161037324 added table building functionality to js and layout for add_prod_cart.html page

---
+++ UI/js/storeManager.js
@@ -0,0 +1,16 @@
+
+function addToCart(){
+    document.getElementById('order_record').style.visibility = 'visible';
+    let prod = document.getElementById('productList').value;
+    let quant = document.getElementById('quantity').value;
+    let table = document.getElementById('tb').getElementsByTagName('tbody')[0];
+    let row = table.insertRow(table.rows.length);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    cell1.innerHTML = prod;
+    cell2.innerHTML = 'Ksh 20.00';
+    cell3.innerHTML = quant;
+    return false;
+    //document.getElementById('order_record').style.visibility = visible;
+}
